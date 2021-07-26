c707d30355d196cda0ea6ad9c07498f4257b500c
Aamod Varma <aamodvarma@gmail.com>
2021-07-26 12:52:09 +0400

    Removed uncecesary files

--- extracttitle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat allfilms | grep "<td><i><a" >> allfilmtitle
-cat allfilmtitle | sed -e 's/<\/a><\/i>//g' -e 's/<\/td>//g'  -e 's/\// /g' -e 's/"//g' -e 's/_(film)//g' | awk '{print $4}' > allfilms
-cat allfilms | sed -e 's/_/ /g' > allfilmtitle
-sudo rm -rf ./allfilms
